e24e93f9ea2fd7f4ef2560cb4f7c40ae2f7ae5fb
Neeze <phanvanhainam.2019@gmail.com>
2024-10-10 17:25:47 +0700

    Refactor GroupedQueryAttention module to apply attention refinement mechanism if provided

--- Pos_Former/model/transformer/group_query_attention.py
+++ Pos_Former/model/transformer/group_query_attention.py
@@ -181,14 +181,14 @@ class GroupedQueryAttention(nn.Module):
         if attn_mask is not None:
             attn_scores = attn_scores.masked_fill(attn_mask == 1, float('-inf'))
 
-        # # Apply ARM if provided
-        # if arm is not None and target_vocab is not None:
-        #     attention_refine = arm(rearrange(attn_scores, "b n t s -> (b n) t s"), target_vocab)
-        #     attention_refine_reshape = rearrange(attention_refine, "(b n) t s -> b n t s", b=b)
-        #     attn_output_weights = attn_scores - attention_refine_reshape
-        # else:
-        #     attn_output_weights = attn_scores
         attn_output_weights = attn_scores
+        # Apply ARM if provided
+        if arm is not None and target_vocab is not None:
+            attention_refine = arm(rearrange(attn_scores, "b n t s -> (b n) t s"), target_vocab)
+            attention_refine_reshape = rearrange(attention_refine, "(b n) t s -> b n t s", b=b)
+            attn_output_weights -= attention_refine_reshape
+        else:
+            attn_output_weights = attn_scores
 
         # Apply softmax and dropout
         attn_output_weights = F.softmax(attn_output_weights, dim=-1)
