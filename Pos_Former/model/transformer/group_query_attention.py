f913442741f02f1157049a14f5734f1a086b3b32
Neeze <phanvanhainam.2019@gmail.com>
2024-10-13 14:23:14 +0700

    Refactor: GroupedQueryAttention module to adjust output dimension and handle head dimension constraints

--- Pos_Former/model/transformer/group_query_attention.py
+++ Pos_Former/model/transformer/group_query_attention.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from typing import Optional, Tuple
 from .arm import AttentionRefinementModule
 from torch import Tensor
-from einops import rearrange
+from einops import rearrange, einsum
 
 class GroupedQueryAttention(nn.Module):
     def __init__(
@@ -22,6 +22,15 @@ class GroupedQueryAttention(nn.Module):
         self.d_out = d_out
         self.num_heads = num_heads
         self.head_dim = d_out // num_heads
+
+        if not self.head_dim % 8 == 0:
+            raise ValueError(
+                f"head_dim (embed_dim / num_heads = {self.head_dim}) must be divisible by 8"
+            )
+        if not self.head_dim <= 128:
+            raise ValueError(
+                f"head_dim (embed_dim / num_heads = {self.head_dim}) must be <= 128"
+            )
 
         self.W_key = nn.Linear(d_in, num_kv_groups * self.head_dim, bias=False)
         self.W_value = nn.Linear(d_in, num_kv_groups * self.head_dim, bias=False)
@@ -78,13 +87,39 @@ class GroupedQueryAttention(nn.Module):
         values = values.view(b, -1, self.num_kv_groups, self.head_dim) # Shape: (b, num_tokens, num_kv_groups, head_dim)
 
         # Transpose keys, values, and queries
-        keys = keys.transpose(1, 2)  # Shape: (b, num_heads, num_tokens, head_dim)
-        values = values.transpose(1, 2)  # Shape: (b, num_heads, num_tokens, head_dim)
-        queries = queries.transpose(1, 2)  # Shape: (b, num_query_groups, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)  # Shape: (b, num_kv_groups, num_tokens, head_dim)
+        values = values.transpose(1, 2)  # Shape: (b, num_kv_groups, num_tokens, head_dim)
+        queries = queries.transpose(1, 2)  # Shape: (b, num_heads, num_tokens, head_dim)
+
+        bq, hq, nq, dq = queries.shape
+        bk, hk, nk, dk = keys.shape
+        bv, hv, nv, dv = values.shape
+
+        if not (bq == bk == bv and dq == dk == dv):
+            raise ValueError(
+                "Expected query, key, and value to have the same batch size (dim=0) and "
+                f"embedding dimension (dim=3), but got query: {query.shape}, "
+                f"key: {key.shape}, and value: {value.shape}."
+            )
+        elif (hk != hv) or (nk != nv):
+            raise ValueError(
+                "Expected key and value to have the same size in dimensions 1 and 2, but "
+                f"got key: {key.shape} and value: {value.shape}."
+            )
+        elif hq % hk != 0:
+            raise ValueError(
+                "Expected query heads to be a multiple of key/value heads, but got "
+                f"query: {query.shape} and key/value: {key.shape}."
+            )
+
+        num_head_groups = hq // hk
+        queries = rearrange(queries, "b (h g) n d -> b g h n d", g=num_head_groups)
 
         # Expand keys and values to match the number of heads
-        keys = keys.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
-        values = values.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
+        # keys = keys.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
+        # values = values.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
+
+
          # For example, before repeat_interleave along dim=1 (query groups):
         #   [K1, K2]
         # After repeat_interleave (each query group is repeated group_size times):
@@ -94,7 +129,10 @@ class GroupedQueryAttention(nn.Module):
         # Compute scaled dot-product attention (aka self-attention) with a causal mask
         # Shape: (b, num_heads, num_tokens, num_tokens)
         # Compute scaled dot-product attention
-        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+        # attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        attn_scores = einsum(queries, keys, "b g h n d, b h s d -> b g h n s")
+        attn_scores = rearrange(attn_scores, "b g h n s -> b (g h) n s")
 
         # Handle key_padding_mask
         if key_padding_mask is not None:
@@ -108,20 +146,24 @@ class GroupedQueryAttention(nn.Module):
 
         # Apply softmax and dropout
         attn_output_weights = F.softmax(attn_scores, dim=-1)
-        # attn_output_weights = self.dropout(attn_output_weights)
+        attn_output_weights = self.dropout(attn_output_weights)
 
-        # if arm is not None and target_vocab is not None:
-        #     attention_refine = arm(curr_attn = rearrange(attn_scores, "b n t s -> (b n) t s"), 
-        #                            tgt_vocab = target_vocab)
-        #     attention_refine_reshape = rearrange(attention_refine, "(b n) t s -> b n t s", b=b)
-        #     attn_output_weights -= attention_refine_reshape
+        if arm is not None and target_vocab is not None:
+            attention_refine = arm(curr_attn = rearrange(attn_scores, "b n t s -> (b n) t s"), 
+                                   tgt_vocab = target_vocab)
+            attention_refine_reshape = rearrange(attention_refine, "(b n) t s -> b n t s", b=b)
+            attn_output_weights -= attention_refine_reshape
 
-        context_vec = (attn_output_weights @ values).transpose(1, 2) # Shape: (b, num_tokens, num_heads, head_dim)
+        attn_output_weights = rearrange(attn_output_weights, "b (g h) n s -> b g h n s", h=self.num_kv_groups)
+        context_vec = einsum(attn_output_weights, values, "b g h n s, b h s d -> b g h n d")
 
+        # context_vec = (attn_output_weights @ values).transpose(1, 2) # Shape: (b, num_tokens, num_heads, head_dim)
         # context_vec = context_vec.transpose(1, 2)
 
         # Combine heads, where self.d_out = self.num_heads * self.head_dim
-        context_vec = context_vec.reshape(b, num_tokens, self.d_out)
+        # context_vec = context_vec.reshape(b, num_tokens, self.d_out)
+
+        context_vec = rearrange(context_vec, "b g h n d -> b n (g h d)")
         attn_output = self.out_proj(context_vec)
 
         attn_output_weights = rearrange(attn_output_weights, "b n t s -> (b n) t s")
